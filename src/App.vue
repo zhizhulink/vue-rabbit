728ae153cf43af28a6c2a093c4e066c20cd6b894
huang <3217376512@qq.com>
2023-10-24 22:37:02 +0800

    home-图片懒加载的实现

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,17 @@
 </script>
 
 <template>
-  <el-button type="primary">Primary</el-button>
+  <!-- 一级路由出口 -->
+  <RouterView/>
+  <div class="test">
+    test css
+  </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.test{
+  color:$priceColor
+}
 header {
   line-height: 1.5;
   max-height: 100vh;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +5,32 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { useIntersectionObserver } from '@vueuse/core'
+//引入初始化样式文件
+import '@/styles/common.scss'
+
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+app.directive('img-lazy', {
+    mounted(el, binding) {
+        // el：指令绑定的那个元素 img
+        // binding：binding.value 指令等于号后面绑定的表达式的值 图片url
+        console.log(el, binding.value)
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+                console.log(isIntersecting)
+                if (isIntersecting) {
+                    // 进入视口区域
+                    el.src = binding.value
+                }
+            }
+        )
+    }
+})
