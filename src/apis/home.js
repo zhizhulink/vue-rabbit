5e5ff22a6f0b87c9a1c6e11e209ca1c3fd695aa7
huang <3217376512@qq.com>
2023-10-25 14:20:52 +0800

    一级分类-路由跳转

--- src/apis/home.js
+++ src/apis/home.js
@@ -1,11 +1,11 @@
-import  httpInstance from '@/utils/http'
+import httpInstance from '@/utils/http'
 
 // 获取banner
 
 export function getBannerAPI() {
-    return httpInstance({
-        url:'/home/banner'
-    })
+  return httpInstance({
+    url: '/home/banner'
+  })
 }
 
 /**
@@ -14,18 +14,29 @@ export function getBannerAPI() {
  * @return {*}
  */
 export const findNewAPI = () => {
-    return httpInstance({
-      url:'/home/new'
-    })
-  }
+  return httpInstance({
+    url: '/home/new'
+  })
+}
 
-  /**
- * @description: 获取人气推荐
- * @param {*}
- * @return {*}
- */
+/**
+* @description: 获取人气推荐
+* @param {*}
+* @return {*}
+*/
 export const getHotAPI = () => {
-    return  httpInstance({
-        url:'home/hot'
-    })
-  }
+  return httpInstance({
+    url: 'home/hot'
+  })
+}
+
+/**
+* @description: 获取所有商品模块
+* @param {*}
+* @return {*}
+*/
+export const getGoodsAPI = () => {
+  return httpInstance({
+    url: '/home/goods'
+  })
+}
