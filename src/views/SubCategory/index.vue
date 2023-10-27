e4366d0aec8e6d39bb11f21482f928d492449dc5
huang <3217376512@qq.com>
2023-10-27 20:20:54 +0800

    二级分类-列表无限加载实现

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -27,6 +27,28 @@ const getGoodList = async() => {
 }
 
 onMounted(()=>getGoodList())
+
+// tab切换回调
+const tabChange =()=>{
+  console.log('tab切换了',reqData.value.sortField)
+  reqData.value.page = 1
+  getGoodList()
+}
+
+// 加载更多
+const disabled = ref(false)
+const load =async()=>{
+  console.log('加载更多数据')
+  // 获取下一页数据
+  reqData.value.page++
+  const res = await getSubCategoryAPI(reqData.value)
+  // goodList.value = [...goodList.value,...res.result.items]
+  goodList.value =goodList.value.concat(res.result.items)
+  // 加载完毕 结束监听
+  if(res.result.items.length === 0){
+    disabled.value = true
+  }
+}
 </script>
 
 <template>
