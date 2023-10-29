727dfaee0edadb9cfec3d97e56d0314fb47e2294
huang <3217376512@qq.com>
2023-10-29 20:59:32 +0800

    放大镜效果-滑块跟随移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { useMouseInElement } from "@vueuse/core";
 // 图片列表
 const imageList = [
   "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -10,24 +11,46 @@ const imageList = [
 ]
 
 // 1.小图切换大图显示
-const activeIndex =ref(0)
-const enterhandle= (i) =>{
+const activeIndex = ref(0)
+const enterhandle = (i) => {
   activeIndex.value = i
 }
+
+// 2、获取鼠标相对位置
+const left = ref(0)
+const top = ref(0)
+const target = ref(null)
+const { elementX, elementY, isOutside } = useMouseInElement(target)
+watch([elementX, elementY], () => {
+  console.log('xy变化了')
+  if (elementX.value > 100 && elementX.value < 300 ){
+    left.value = elementX.value - 100
+  }
+  if( elementY.value > 100 && elementY.value < 300) {
+      top.value = elementY.value - 100
+  }
+
+  if (elementX.value > 300) {left.value = 200}
+  if(elementX.value < 100){left.value = 0}
+  if(elementY.value > 300){top.value = 200}
+  if(elementY.value < 100){top.value = 0}
+})
+
 </script>
 
 
 <template>
+  {{ elementX }},{{ elementY }},{{ isOutside }}
   <div class="goods-image">
     <!-- 左侧大图-->
-    <div class="middle" ref="target">
+    <div class="middle"  ref="target">
       <img :src="imageList[activeIndex]" alt="" />
-      <!-- 蒙层小滑块 -->
-      <div class="layer" :style="{ left: `0px`, top: `0px` }"></div>
+      <!-- 蒙层小滑块 --> 
+      <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
     </div>
     <!-- 小图列表 -->
     <ul class="small">
-      <li v-for="(img, i) in imageList" :key="i" @mouseenter="enterhandle(i)" :class="{active:activeIndex===i}">
+      <li v-for="(img, i) in imageList" :key="i" @mouseenter="enterhandle(i)" :class="{ active: activeIndex === i }">
         <img :src="img" alt="" />
       </li>
     </ul>
