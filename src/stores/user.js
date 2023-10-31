3208f8f04daa0db530d621fa23f78094220f48ac
huang <3217376512@qq.com>
2023-10-31 16:18:43 +0800

    登录-退出登录业务实现

--- src/stores/user.js
+++ src/stores/user.js
@@ -11,10 +11,16 @@ export const useUserStore = defineStore('user', () => {
         const res = await loginAPI({ account, password })
         userInfo.value = res.result
     }
+    // 退出是清除用户信息
+    const clearUserInfo = () => {
+        userInfo.value = {}
+    }
+
     // 3.以对象的格式把state和action return出去
     return {
         userInfo,
-        getUserInfo
+        getUserInfo,
+        clearUserInfo
     }
 },
     {
