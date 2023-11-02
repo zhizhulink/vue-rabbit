7e5d1db7b16ffd3086476f220676294a999884d9
huang <3217376512@qq.com>
2023-11-02 20:13:47 +0800

    接口购物车-加入购物车实现

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,23 +1,37 @@
 // 封装购物车模块
-import { ref ,computed} from 'vue'
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import { useUserStore } from './user'
+import {insertCartAPI,findNewCartAPI} from '@/apis/cart'
 
 export const useCartStore = defineStore('cart', () => {
+    const userStore = useUserStore()
+    const isLogin = computed(() => userStore.userInfo.token)
+
     // 1、定义state - cartList
     const cartList = ref([])
-
     // 2、定义action-addCart
-    const addCart = (goods) => {
-        // 3.添加购物车操作
-        // 已添加过 - count + 1
-        // 没有添加过 - 直接push
-        // 思路：通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
-        const item = cartList.value.find((item) => item.skuId === goods.skuId)
-        if (item) {
-            item.count++
+    const addCart = async(goods) => {
+        const {skuId,count} = goods
+        if (isLogin.value) {
+            //登录之后的加入购物车
+            await insertCartAPI({skuId,count})
+           const res = await findNewCartAPI()
+           cartList.value = res.result
+           
         } else {
-            cartList.value.push(goods)
+            // 3.添加购物车操作
+            // 已添加过 - count + 1
+            // 没有添加过 - 直接push
+            // 思路：通过匹配传递过来的商品对象中的skuId能不能在cartList中找到，找到了就是添加过
+            const item = cartList.value.find((item) => item.skuId === goods.skuId)
+            if (item) {
+                item.count++
+            } else {
+                cartList.value.push(goods)
+            }
         }
+
     }
     // 删除购物车
     const delCart = (skuId) => {
@@ -32,8 +46,8 @@ export const useCartStore = defineStore('cart', () => {
     }
 
     // 单选功能
-    const singleCheck=(skuId,selected)=>{
-        const item =cartList.value.find((item)=>item.skuId===skuId)
+    const singleCheck = (skuId, selected) => {
+        const item = cartList.value.find((item) => item.skuId === skuId)
         item.selected = selected
     }
 
@@ -44,17 +58,17 @@ export const useCartStore = defineStore('cart', () => {
         })
     }
     // 已选择数量
-    const selectedCount = computed(()=>cartList.value.filter((item)=>item.selected).reduce((total,item)=>total+item.count,0))
+    const selectedCount = computed(() => cartList.value.filter((item) => item.selected).reduce((total, item) => total + item.count, 0))
     // 已选择商品合计
-    const selectedPrice = computed(()=>cartList.value.filter((item)=>item.selected).reduce((total,item)=>total+item.count*item.price,0))
+    const selectedPrice = computed(() => cartList.value.filter((item) => item.selected).reduce((total, item) => total + item.count * item.price, 0))
 
     // 计算属性
     // 1.总的数量 所有项的count之和
-    const allCount = computed(()=>cartList.value.reduce((total,item)=>total+item.count,0))
+    const allCount = computed(() => cartList.value.reduce((total, item) => total + item.count, 0))
     // 2、总价 所有项的count*price之和
-    const allPrice = computed(()=>cartList.value.reduce((total,item)=>total+item.count*item.price,0))
+    const allPrice = computed(() => cartList.value.reduce((total, item) => total + item.count * item.price, 0))
 
-    const isAll =computed(()=>cartList.value.every((item)=>item.selected))
+    const isAll = computed(() => cartList.value.every((item) => item.selected))
 
     return {
         cartList,
