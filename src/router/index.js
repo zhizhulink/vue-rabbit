cece70beaeef7807a2608fec6f616a61142f1646
huang <3217376512@qq.com>
2023-11-05 20:56:50 +0800

    会员中心-个人中心信息渲染

--- src/router/index.js
+++ src/router/index.js
@@ -12,58 +12,74 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   // 配置path和component对应关系的位置
   routes: [
     {
-      path:'/',
-      component:Layout,
-      children:[
+      path: '/',
+      component: Layout,
+      children: [
         {
-          path:'',
-          component:Home
+          path: '',
+          component: Home
         },
         {
-          path:'category/:id',
-          component:Category
+          path: 'category/:id',
+          component: Category
         },
         {
-          path:'category/sub/:id',
-          component:SubCategory
+          path: 'category/sub/:id',
+          component: SubCategory
         },
         {
-          path:'detail/:id',
-          component:Detail
+          path: 'detail/:id',
+          component: Detail
         },
         {
-          path:'cartlist',
-          component:CartList
+          path: 'cartlist',
+          component: CartList
         },
         {
-          path:'checkout',
-          component:Checkout
+          path: 'checkout',
+          component: Checkout
         },
         {
-          path:'pay',
-          component:Pay
+          path: 'pay',
+          component: Pay
         },
         {
-          path:'paycallback',
-          component:PayBack
+          path: 'paycallback',
+          component: PayBack
+        },
+        {
+          path: 'member',
+          component: Member,
+          children: [
+            {
+              path: 'user',
+              component: UserInfo
+            },
+            {
+              path: 'order',
+              component: UserOrder
+            }
+          ]
+        },
+        {
+          path: '/login',
+          component: Login
         }
-      ]
-    },
-    {
-      path:'/login',
-      component:Login
+      ],
+      // 路由行为配置项
+      scrollBehavior() {
+        return { top: 0 }
+      }
     }
-  ],
-  // 路由行为配置项
-  scrollBehavior(){
-    return { top:0}
-  }
-})
+  ]})
 
 export default router
